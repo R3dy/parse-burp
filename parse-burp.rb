bd53fa3f1e21b6db5645a3debed4c4f48d7f277f
Royce Davis <r3dy@Royces-MacBook-Pro.local>
2013-09-09 20:46:21 -0500

    fixed more issues with blank responses

--- parse-burp.rb
+++ parse-burp.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby
 # Copyright (C) 2013 www.pentestgeek.com Royce Davis (@r3dy__)
-# #
-# #This program is free software: you can redistribute it and/or modify
-# #it under the terms of the GNU General Public License as published by
-# #the Free Software Foundation, either version 3 of the License, or
-# #any later version.
-# #
-# #This program is distributed in the hope that it will be useful,
-# #but WITHOUT ANY WARRANTY; without even the implied warranty of
-# #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# #GNU General Public License for more details.
-# #
-# #You should have received a copy of the GNU General Public License
-# #along with this program. If not, see <http://www.gnu.org/licenses/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
 # #
 require 'nokogiri'
 
@@ -51,8 +51,8 @@ def clean_finding(finding)
   output << finding.css('name').text + "\t"
   output << finding.css('issueBackground').text + "\t"
   output << finding.css('remediationBackground').text + "\t"
-  if finding.at_css('response') && !finding.css('response').text == nil
-    output << finding.css('response').text.split("Server:")[1].split("\r\n")[0] + "\t"
+  if finding.css('response').text.include?("Server:")
+    output << finding.css('response').text.split("Server: ")[1].split("\n")[0] + "\t"
   else
     output << "\t"
   end
